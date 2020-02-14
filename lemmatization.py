7efeb818b59bf643f61b43690d45bb5f5e299e5b
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-13 18:47:13 -0800

    Created outline of lemmatization functions

---
+++ lemmatization.py
@@ -0,0 +1,29 @@
+
+# lemmatize a string of ascii characters
+def lemmatization(word: str) -> str:
+    pass
+
+
+# Step 1 of lemmatization as outlined in the slides
+def _step1(word: str) -> str:
+    pass
+
+
+# Step 2 of lemmatization as outlined in the slides
+def _step2(word: str) -> str:
+    pass
+
+
+# Step 3 of lemmatization as outlined in the slides
+def _step3(word: str) -> str:
+    pass
+
+
+# Step 4 of lemmatization as outlined in the slides
+def _step4(word: str) -> str:
+    pass
+
+
+# Step 5 of lemmatization as outlined in the slides
+def _step5(word: str) -> str:
+    pass
