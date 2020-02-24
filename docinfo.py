efc34d3852d8434d644bee1b39b01d3505a34905
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-23 18:54:18 -0800

    Added caculation function for tf-idf

--- docinfo.py
+++ docinfo.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import math
 
 
 class DocInfo:
@@ -6,3 +7,21 @@ class DocInfo:
         self._docid = docid
         self._term_frequency = term_frequency
         self._relative_importance = relative_importance
+
+        self._normalized_term_frequency = self._calculate_normalized_term_frequency(self._term_frequency)
+
+        self._document_frequency = 0
+
+        self._inverse_document_frequency = 0.0
+
+
+    def _calculate_normalized_term_frequency(self, tf) -> float:
+        if tf > 0:
+            return 1 + math.log(tf, 10)
+        return 0.0
+
+    def _calculate_inverse_document_frequency(self, N, df) -> float:
+        self._inverse_document_frequency =  math.log((N/df), 10)
+
+    def tfidf(self) -> float:
+        return self._normalized_term_frequency * self._inverse_document_frequency
